b156d40816d250c1947cd0ecc231509a450a5c8c
tde-99 <deltaentertainmentofc@gmail.com>
2025-07-06 00:51:28 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,3 @@
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 from pyrogram import filters, Client, errors, enums
 from pyrogram.errors import UserNotParticipant
@@ -17,22 +13,37 @@ app = Client(
     bot_token=cfg.BOT_TOKEN
 )
 
-#━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ Main process ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+pending_users = {}
 
 @app.on_chat_join_request(filters.group | filters.channel)
-async def approve(_, m : Message):
+async def approve(_, m: Message):
     op = m.chat
     kk = m.from_user
     try:
         add_group(m.chat.id)
-        await app.approve_chat_join_request(op.id, kk.id)
-        await app.send_message(kk.id, "**Hello {}!\nWelcome To {}\n\n__Powerd By : @VJ_Botz __**".format(m.from_user.mention, m.chat.title))
         add_user(kk.id)
-    except errors.PeerIdInvalid as e:
-        print("user isn't start bot(means group)")
+
+        share_link = f"https://t.me/share/url?url=%E0%B4%AE%E0%B5%80%E0%B4%A1%E0%B4%BF%E0%B4%AF%20%E0%B4%AA%E0%B5%8B%E0%B4%B8%E0%B5%8D%E0%B4%B1%E0%B5%8D%E0%B4%B1%E0%B5%8D%20%E0%B4%9A%E0%B5%86%E0%B4%AF%E0%B5%8D%E0%B4%AF%E0%B5%81%E0%B4%A8%E0%B5%8D%E0%B4%A8%E0%B4%B5%E0%B5%BC%20%E0%B4%AE%E0%B4%BE%E0%B4%A4%E0%B5%8D%E0%B4%B0%E0%B4%82%20%E0%B4%95%E0%B4%AF%E0%B4%B1%E0%B4%BF%E0%B4%95%E0%B5%8D%E0%B4%95%E0%B5%8B%20%0A%F0%9F%93%A5%20%20t.me/%2BsVf-EO-XSuA0NzY1%20%F0%9F%93%A5%0A%F0%9F%93%A5%C2%A0%20t.me/%2BsVf-EO-XSuA0NzY1%20%F0%9F%93%A5"
+        pending_users[kk.id] = op.id
+
+        await app.send_message(
+            kk.id,
+            f"🚨 To join **{op.title}**,**ഇതു മീഡിയ പോസ്റ്റിങ് ഉള്ള ഗ്രൂപ്പ് ആണ് ഇപ്പോൾ മീഡിയ ഗ്രൂപ്പുകൾ തുടർച്ചയായി ബാൻ ആവുന്നതുകൊണ്ടു  ഈ ഗ്രൂപ്പിന്റെ ലിങ്ക് 3 പേർക്ക് അയച്ചു കൊടുത്താൽ മാത്രമേ Approove ആവുകയുള്ളൂ**.",
+            reply_markup=InlineKeyboardMarkup([
+                [InlineKeyboardButton("Share Now", url=share_link)],
+                [InlineKeyboardButton("Try Again", callback_data="try_join_again")]
+            ])
+        )
+    except errors.PeerIdInvalid:
+        print("User has not started the bot yet.")
     except Exception as err:
-        print(str(err))    
- 
+        print(str(err))
+
+@app.on_callback_query(filters.regex("try_join_again"))
+async def try_again_check(_, cb: CallbackQuery):
+    user_id = cb.from_user.id
+
+    await cb.answer("❗️ 𝐍𝐨 𝐔𝐬𝐞𝐫 𝐉𝐨𝐢𝐧𝐞𝐝 𝐁𝐲 𝐘𝐨𝐮𝐫 𝐋𝐢𝐧𝐤𝐬 𝐒𝐡𝐚𝐫𝐞 𝐀𝐠𝐚𝐢𝐧 𝐀𝐧𝐝 𝐓𝐫𝐲 𝐀𝐠𝐚𝐢𝐧 ❗️", show_alert=True)
 #━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ Start ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
 @app.on_message(filters.private & filters.command("start"))
@@ -55,12 +66,12 @@ async def op(_, m :Message):
         return 
     keyboard = InlineKeyboardMarkup(
         [[
-            InlineKeyboardButton("🗯 Channel", url="https://t.me/vj_botz"),
-            InlineKeyboardButton("💬 Support", url="https://t.me/vj_bot_disscussion")
+            InlineKeyboardButton("⚜️ 𝐎𝐖𝐍𝐄𝐑  ⚜️", url="https://t.me/CLUBXA"),
+            InlineKeyboardButton("𝐃𝐄𝐕 🧑‍💻", url="https://t.me/DUNEBOTS")
         ]]
     )
     add_user(m.from_user.id)
-    await m.reply_photo("https://graph.org/file/d57d6f83abb6b8d0efb02.jpg", caption="**🦊 Hello {}!\nI'm an auto approve [Admin Join Requests]({}) Bot.\nI can approve users in Groups/Channels.Add me to your chat and promote me to admin with add members permission.\n\n__Powered By : @VJ_Botz __**".format(m.from_user.mention, "https://t.me/telegram/153"), reply_markup=keyboard)
+    await m.reply_photo("https://graph.org/file/fa9a4b4f0b1078f2c4dbe-bdd9ea0c0eda33117c.jpg", caption="**🦊 Hello {}!\nI'm an auto approve [Admin Join Requests]({}) Bot.\nI can approve users in Groups/Channels.Add me to your chat and promote me to admin with add members permission.\n\n__Powered By : @CLUBXA __**".format(m.from_user.mention, "https://t.me/telegram/153"), reply_markup=keyboard)
     
 
 #━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ callback ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
@@ -74,12 +85,12 @@ async def chk(_, cb : CallbackQuery):
         return 
     keyboard = InlineKeyboardMarkup(
         [[
-            InlineKeyboardButton("🗯 Channel", url="https://t.me/vj_botz"),
-            InlineKeyboardButton("💬 Support", url="https://t.me/vj_bot_disscussion")
+            InlineKeyboardButton("⚜️ 𝐎𝐖𝐍𝐄𝐑  ⚜️", url="https://t.me/CLUBXA"),
+            InlineKeyboardButton("𝐃𝐄𝐕 🧑‍💻", url="https://t.me/DUNEBOTS")
         ]]
     )
     add_user(m.from_user.id)
-    await cb.edit_text(text="**🦊 Hello {}!\nI'm an auto approve [Admin Join Requests]({}) Bot.\nI can approve users in Groups/Channels.Add me to your chat and promote me to admin with add members permission.\n\n__Powered By : @VJ_Botz __**".format(cb.from_user.mention, "https://t.me/telegram/153"), reply_markup=keyboard)
+    await cb.edit_text(text="**🦊 Hello {}!\nI'm an auto approve [Admin Join Requests]({}) Bot.\nI can approve users in Groups/Channels.Add me to your chat and promote me to admin with add members permission.\n\n__Powered By : @CLUBXA __**".format(cb.from_user.mention, "https://t.me/telegram/153"), reply_markup=keyboard)
     
 
 #━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ info ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
